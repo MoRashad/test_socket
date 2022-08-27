a65e0945ff9f5bd3637555ed27911ad48c1f964e
MoRashad <44236713+MoRashad@users.noreply.github.com>
2022-08-27 16:31:06 +0200

    icp price

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 const request = require("request");
 const socketio = require("socket.io");
+const Contract = require("web3-eth-contract");
 const express = require("express");
 const http = require("http");
+const pkgAgent = require("@dfinity/agent");
+const { HttpAgent, Actor } = pkgAgent;
+
 let coinbaseBtc, coinbaseEth, binanceBtc, binanceEth, avgBtc, avgEth;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-setInterval(() => {
+const idlFactory = ({ IDL }) => {
+  const PriceData = IDL.Record({
+    signature: IDL.Text,
+    provider: IDL.Text,
+    asset: IDL.Nat32,
+    timestamp: IDL.Text,
+    is_closed: IDL.Bool,
+    price: IDL.Text,
+  });
+  return IDL.Service({
+    add_data: IDL.Func([IDL.Nat32, PriceData], [IDL.Bool], []),
+    add_node: IDL.Func([IDL.Principal], [IDL.Opt(IDL.Principal)], []),
+    get_caller: IDL.Func([], [IDL.Principal], ["query"]),
+    get_data: IDL.Func([IDL.Nat32], [IDL.Vec(PriceData)], ["query"]),
+    get_nodes: IDL.Func([], [IDL.Vec(IDL.Principal)], ["query"]),
+    get_owner: IDL.Func([], [IDL.Opt(IDL.Principal)], ["query"]),
+    remove_node: IDL.Func([IDL.Principal], [IDL.Opt(IDL.Principal)], []),
+  });
+};
+
+async function getICPPrice(pair) {
+  const canisterId = "ffs6z-fyaaa-aaaao-aai6q-cai";
+
+  const host = "https://ic0.app";
+
+  const agent = new HttpAgent({ fetch, host });
+
+  const actor = Actor.createActor(idlFactory, {
+    agent,
+    canisterId,
+  });
+
+  var f = await actor.get_data(pair);
+
+  return f;
+}
+
+async function work(pairIndex) {
+  var icp_data = await getICPPrice(pairIndex);
+  // console.log(icp_data[0].price / 1e18);
+  return icp_data[0].price / 1e18;
+}
+
+setInterval(async () => {
   request(
     "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT",
     function (error, response, body) {
@@ -47,12 +94,16 @@ setInterval(() => {
 
     console.log("ETH price :  " + avgEth);
     console.log("\n");
-    io.emit("price", { avgBtc, avgEth });
+    let icpBtc = await work(0);
+    console.log(icpBtc);
+    let icpEth = await work(1);
+    console.log(icpEth);
+    io.emit("price", { avgBtc, avgEth, icpBtc, icpEth });
   }
 }, 1000);
-io.on("connection", (socket) => {
-  console.log("user connected", socket);
-});
+// io.on("connection", (socket) => {
+//   console.log("user connected", socket);
+// });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
